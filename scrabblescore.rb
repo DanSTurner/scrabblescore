6f3a3621ec2a852da6eb681f62f3d56d603abc11
Dan S Turner <turner@nowwithmoredan.com>
2013-11-01 15:07:46 -0700

    cleaned up

--- scrabblescore.rb
+++ scrabblescore.rb
@@ -1,18 +1,14 @@
 class Scrabblescore
   def initialize
-    @numlist = {'a'=>1,'b'=>3,'c'=>3,'d'=>2,'e'=>1,'f'=>4,'g'=>2,'h'=>4,'i'=>1,'j'=>8,'k'=>5,'l'=>1,'m'=>3,'n'=>1,'o'=>1,'p'=>3,'q'=>10,'r'=>1,'s'=>1,'t'=>1,'u'=>1,'v'=>4,'w'=>4,'x'=>8,'y'=>4,'z'=>10,}
-    @word = ''
+    @lettervalue = {'a'=>1,'b'=>3,'c'=>3,'d'=>2,'e'=>1,'f'=>4,'g'=>2,'h'=>4,'i'=>1,'j'=>8,'k'=>5,'l'=>1,'m'=>3,'n'=>1,'o'=>1,'p'=>3,'q'=>10,'r'=>1,'s'=>1,'t'=>1,'u'=>1,'v'=>4,'w'=>4,'x'=>8,'y'=>4,'z'=>10,}
   end
 
-
-
-  def wordgetter
-    puts "word to be scored?"
-    word = gets.chomp
+  def scorer(word)
+    score = 0
+    word.downcase.gsub(/[^a-z]/, '').scan(/./).each { |letter| score += @lettervalue[letter] }
+    puts "your score is #{@score}"
+    return score
   end
 
 
 end
-
-  a = Scrabblescore.new
-  a.wordgetter
--- spec/scrabblescore_spec.rb
+++ spec/scrabblescore_spec.rb
@@ -6,12 +6,8 @@ describe 'Scorer' do
     @wrd = Scrabblescore.new
   end
 
-  it 'should get a word' do
-    @wrd.wordgetter.should_not be nil
-  end
-
   it 'should return 4 for the word test' do
-    wrd('test').should eq 4
+    @wrd.scorer("Test").should eq 4
   end
 
 end
